c0bcb21434a89f2c28ac63aebc92098e38791964
chicki <55694044+chickiyeah@users.noreply.github.com>
2026-03-14 13:59:11 +0900

    fix redirect

--- src/main/java/com/deco/yakbang/DecoApplication.java
+++ src/main/java/com/deco/yakbang/DecoApplication.java
@@ -3,12 +3,10 @@ package com.deco.yakbang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//SecurityAutoConfiguration을 제외하면 리다이렉트가 사라집니다.
+@SpringBootApplication(exclude = { org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
 public class DecoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DecoApplication.class, args);
-		
-	}
-
+ public static void main(String[] args) {
+     SpringApplication.run(DecoApplication.class, args);
+ }
 }
