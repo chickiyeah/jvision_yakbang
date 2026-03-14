49f20e9bcef6180094b3911c277ad6711667ca34
chicki <55694044+chickiyeah@users.noreply.github.com>
2026-03-14 14:22:23 +0900

    tomcat

--- src/main/java/com/deco/yakbang/DecoApplication.java
+++ src/main/java/com/deco/yakbang/DecoApplication.java
@@ -2,12 +2,20 @@ package com.deco.yakbang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @MapperScan(basePackages = "com.deco.yakbang.service.impl") // Mapper 인터페이스가 있는 패키지 경로를 적으세요
 @SpringBootApplication(exclude = { org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
-public class DecoApplication {
- public static void main(String[] args) {
-     SpringApplication.run(DecoApplication.class, args);
- }
+public class DecoApplication extends SpringBootServletInitializer {
+	// 2. 외장 톰캣 배포를 위한 설정 메서드 추가
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DecoApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecoApplication.class, args);
+    }
 }
